578c761369ff62daa35c19746b0459e8ffebd27e
Luka Ljevar <ll86208@student.uni-lj.si>
2023-08-27 14:17:46 +0200

    Koda za obrezovanje HTML drevesa.

--- BeautifulSoup.py
+++ BeautifulSoup.py
@@ -11,9 +11,38 @@ def obreži_html_drevo(element):
                 prune_tree(otrok)
 
 
-def odstrani_vizualne_element(juha):
+def odstrani_vizualne_elemente(juha):
     for element in juha(['script', 'style', 'link', 'meta', 'noscript', 'img']):
         element.decompose()
 
 def izlušči_relevantne_dive(juha):
     return juha.find('div', {'class': 'category-products'})
+
+podatki = []
+id_produkta = []
+ime_produkta = []
+ocena = []
+opis = []
+razpoložljivost = []
+partner = []
+cena = []
+
+združena_juha = BeautifulSoup('', 'html.parser')
+
+for i in range(1, 7):
+
+    with open(f'stran{i}.html', 'r', encoding = 'utf-8') as f:
+        html_vsebina = f.read()
+
+    juha = BeautifulSoup(html_vsebina, 'html.parser')
+
+    obreži_html_drevo(juha)
+    odstrani_vizualne_elemente(juha)
+
+    relevanten_div = izlušči_relevantne_dive(juha)
+
+    if relevanten_div:
+        združena_juha.body.append(relevanten_div)
+
+    else:
+        print(f"'category_products' div na strani{i} ni bil najden")
