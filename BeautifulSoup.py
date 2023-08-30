0ef0884bb95258ef608e6df8050a989cc61ae5bf
Luka Ljevar <ll86208@student.uni-lj.si>
2023-08-30 10:59:43 +0200

    "Štelio_Ocen" in "Cena_v_Evrih" sem pretvoril v int oz. float objekte.

--- BeautifulSoup.py
+++ BeautifulSoup.py
@@ -1,6 +1,8 @@
-from pathlib import Path
 from bs4 import BeautifulSoup, Tag
 import pandas as pd
+
+pd.set_option( 'display.max_columns', None)
+
 
 def obreži_html_drevo(element):
     for otrok in list(element.children):
@@ -32,9 +34,9 @@ združena_juha = BeautifulSoup('', 'html.parser')
 
 for i in range(1, 7):
 
-    with Path(f'C:/Users/luka/Desktop/Projektna UVP/analiza-podatkov/stran{i}.html').open(mode='r', encoding='utf-8') as f:
+    #with Path(f'C:/Users/luka/Desktop/Projektna UVP/analiza-podatkov/stran{i}.html').open(mode='r', encoding='utf-8') as f:
 
-    #with open(f'stran{i}.html', 'r', encoding = 'utf-8') as f:
+    with open(f'stran{i}.html', 'r', encoding = 'utf-8') as f:
         html_vsebina = f.read()
 
     juha = BeautifulSoup(html_vsebina, 'html.parser')
@@ -47,7 +49,6 @@ for i in range(1, 7):
     if relevanten_div:
         #združena_juha.body.append(relevanten_div)
         združena_juha.append(relevanten_div)
-
     else:
         print(f"'category_products' div na strani{i} ni bil najden")
 
@@ -62,28 +63,34 @@ for blok in bloki:
     ime_produktov.append(ime_produkta.text.strip() if ime_produkta else "N/A" )
 
     ocena = blok.find("span", {"data-testid": "rating-count"})
-    ocene.append(ocena.text.strip() if ocena else "N/A")
+    ocene.append(int(ocena.text.strip().strip("x")) if ocena else "N/A")
 
     opis = blok.find("p")
     opisi.append(opis.text.strip() if opis else "N/A")
 
     razpoložljivost = blok.find("p", {"data-testid": "availability-tooltip-text"})
-    razpoložljivosti.append(razpoložljivost.text.strip() if razpoložljivost else "N/A")
+    razpoložljivosti.append(razpoložljivost.text.strip("na voljo predvidoma:\n        ") if razpoložljivost else "N/A")
 
     partner = blok.find("b")
     partnerji.append(partner.text.strip() if partner else "N/A")
 
     cena = blok.find("span", {"data-sel": "product-box-price"})
-    cene.append(cena.text.strip() if cena else "N/A")
-
-
+    cene.append(float(cena.text.strip().strip("€").strip().replace(".", "").replace(",", ".")) if cena else "N/A")
 
 df = pd.DataFrame({
     "ID_Produkta": id_produktov,
     "Ime_Produkta": ime_produktov,
-    "Ocena": ocene,
+    "Število_Ocen": ocene,
     "Opis": opisi,
     "Razpoložljivost": razpoložljivosti,
     "Partner": partnerji,
-    "Cena": cene
+    "Cena_v_Evrih": cene
 })
+
+#df.to_csv("podatki.csv", sep=";")
+
+# Odstranjevanje &nbps
+df.replace("\xa0", ' ', regex=True, inplace=True)
+df.drop(df.columns[[0]], axis=1, inplace=True)
+# Shrani csv brez &nbps
+df.to_csv("podatki.csv", sep=";")
