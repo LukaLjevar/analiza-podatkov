a13ad3038f795b1b93f85372347715471148874a
Luka Ljevar <ll86208@student.uni-lj.si>
2023-08-31 12:03:04 +0200

    Dodal sem komentarje.

--- Glavni.py
+++ Glavni.py
@@ -2,6 +2,9 @@ from Zajem_Podatkov import zajem_podatkov
 from Juha import izlušči_grobo
 from Juha import izlušči_in_zapiši
 
+#Zajamemo podatke iz šestih strani
 zajem_podatkov(6)
-izlušči_grobo(1,7)
+
+#Podatke izluščimo in jih zapišemo v Pandas DataFrame in csv.
+izlušči_grobo(1, 7)
 izlušči_in_zapiši()
